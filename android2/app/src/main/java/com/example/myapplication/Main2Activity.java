1e71eddf9c5a95a8a2360a322f3dc0da7c5fc151
Kanzeparov <kanzeparov@yandex.ru>
2019-10-27 00:13:42 +0300

    added main app

--- android2/app/src/main/java/com/example/myapplication/Main2Activity.java
+++ android2/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,14 +1,61 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+import org.web3j.crypto.ECDSASignature;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
+import org.web3j.crypto.Sign;
+
+import java.math.BigInteger;
 
 public class Main2Activity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String s = intent.getStringExtra("url");
+        String[] mas = s.split(" ");
+        ImageView imageView = findViewById(R.id.status);
+
+        String addressRecovered = null;
+        boolean match = false;
+        String pas = "password";
+        String owner = "";
+
+        // Iterate for each possible key to recover
+        for (int i = 0; i < 4; i++) {
+            BigInteger publicKey =
+                    Sign.recoverFromSignature(
+                            (byte) i,
+                            new ECDSASignature(
+                                    new BigInteger(1, mas[1].getBytes()), new BigInteger(1, mas[2].getBytes())),
+                            pas.getBytes());
+
+            if (publicKey != null) {
+                addressRecovered = "0x" + Keys.getAddress(publicKey);
+                //String s = "0x" + Keys.getAddress(credentials.getEcKeyPair().getPublicKey());
+                if(owner.equals(addressRecovered)) {
+                    match = true;
+                    break;
+                }
+            }
+        }
+
+        if(match) {
+            imageView.setImageDrawable(getDrawable(R.drawable.ok));
+        }
+
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+
     }
 }
--- android2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    private TextView resultTextView;
+    private QRCodeReaderView qrCodeReaderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrCodeReaderView = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+
+        // Use this function to enable/disable decoding
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(true);
+
+        // Use this function to set front camera preview
+        qrCodeReaderView.setFrontCamera();
+
+        // Use this function to set back camera preview
+        qrCodeReaderView.setBackCamera();
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed in View
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("url",text);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
     }
 }
