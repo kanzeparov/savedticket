9527d5d3555b330b490d027e6dce3ed62b58e88a
AntonPecherkin <38107123+AntonPecherkin@users.noreply.github.com>
2019-10-27 00:02:53 +0300

    Create Ticket721.sol

---
+++ Ticket721.sol
@@ -0,0 +1,535 @@
+pragma solidity ^0.4.20;
+
+
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+ 
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+   
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+   
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+
+library AddressUtils {
+
+    
+    function isContract(address addr) internal view returns (bool) {
+        uint256 size;
+        // XXX Currently there is no better way to check if there is a contract in an address
+        // than to check the size of the code at that address.
+        // See https://ethereum.stackexchange.com/a/14016/36603
+        // for more details about how this works.
+        // TODO Check this again before the Serenity release, because all addresses will be
+        // contracts then.
+        // solium-disable-next-line security/no-inline-assembly
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
+
+}
+
+
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+   
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+  
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+  
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+
+contract ERC721Receiver {
+ 
+    bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
+
+    /**
+     * @notice Handle the receipt of an NFT
+     * @dev The ERC721 smart contract calls this function on the recipient
+     *  after a `safetransfer`. This function MAY throw to revert and reject the
+     *  transfer. This function MUST use 50,000 gas or less. Return of other
+     *  than the magic value MUST result in the transaction being reverted.
+     *  Note: the contract address is always the message sender.
+     * @param _from The sending address
+     * @param _tokenId The NFT identifier which is being transfered
+     * @param _data Additional data with no specified format
+     * @return `bytes4(keccak256("onERC721Received(address,uint256,bytes)"))`
+     */
+    function onERC721Received(address _from, uint256 _tokenId, bytes _data) public returns(bytes4);
+}
+
+
+
+contract ERC721Basic {
+    event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+
+    function balanceOf(address _owner) public view returns (uint256 _balance);
+    function ownerOf(uint256 _tokenId) public view returns (address _owner);
+    function exists(uint256 _tokenId) public view returns (bool _exists);
+
+    function approve(address _to, uint256 _tokenId) public;
+    function getApproved(uint256 _tokenId) public view returns (address _operator);
+
+    function setApprovalForAll(address _operator, bool _approved) public;
+    function isApprovedForAll(address _owner, address _operator)
+    public view returns (bool);
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public;
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes _data) public;
+}
+
+
+
+contract ERC721Enumerable is ERC721Basic {
+    function totalSupply() public view returns (uint256);
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256 _tokenId);
+
+    function tokenByIndex(uint256 _index) public view returns (uint256);
+}
+
+
+
+contract ERC721Metadata is ERC721Basic {
+    function name() public view returns (string _name);
+    function symbol() public view returns (string _symbol);
+    function tokenURI(uint256 _tokenId) public view returns (string);
+}
+
+
+
+contract ERC721 is ERC721Basic, ERC721Enumerable, ERC721Metadata {}
+
+
+
+contract ERC721BasicToken is ERC721Basic {
+    using SafeMath for uint256;
+    using AddressUtils for address;
+
+    // Equals to `bytes4(keccak256("onERC721Received(address,uint256,bytes)"))`
+    // which can be also obtained as `ERC721Receiver(0).onERC721Received.selector`
+    bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
+
+    // Mapping from token ID to owner
+    mapping (uint256 => address) internal tokenOwner;
+
+    // Mapping from token ID to approved address
+    mapping (uint256 => address) internal tokenApprovals;
+
+    // Mapping from owner to number of owned token
+    mapping (address => uint256) internal ownedTokensCount;
+
+    // Mapping from owner to operator approvals
+    mapping (address => mapping (address => bool)) internal operatorApprovals;
+
+    modifier onlyOwnerOf(uint256 _tokenId) {
+        require(ownerOf(_tokenId) == msg.sender);
+        _;
+    }
+
+    modifier canTransfer(uint256 _tokenId) {
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        _;
+    }
+
+   function balanceOf(address _owner) public view returns (uint256) {
+        require(_owner != address(0));
+        return ownedTokensCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns (address) {
+        address owner = tokenOwner[_tokenId];
+        require(owner != address(0));
+        return owner;
+    }
+
+    function exists(uint256 _tokenId) public view returns (bool) {
+        address owner = tokenOwner[_tokenId];
+        return owner != address(0);
+    }
+
+    function approve(address _to, uint256 _tokenId) public {
+        address owner = ownerOf(_tokenId);
+        require(_to != owner);
+        require(msg.sender == owner || isApprovedForAll(owner, msg.sender));
+
+        if (getApproved(_tokenId) != address(0) || _to != address(0)) {
+            tokenApprovals[_tokenId] = _to;
+            Approval(owner, _to, _tokenId);
+        }
+    }
+
+
+    function getApproved(uint256 _tokenId) public view returns (address) {
+        return tokenApprovals[_tokenId];
+    }
+
+    function setApprovalForAll(address _to, bool _approved) public {
+        require(_to != msg.sender);
+        operatorApprovals[msg.sender][_to] = _approved;
+        ApprovalForAll(msg.sender, _to, _approved);
+    }
+
+
+    function isApprovedForAll(address _owner, address _operator) public view returns (bool)
+    {
+        return operatorApprovals[_owner][_operator];
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public canTransfer(_tokenId)
+    {
+        require(_from != address(0));
+        require(_to != address(0));
+
+        clearApproval(_from, _tokenId);
+        removeTokenFrom(_from, _tokenId);
+        addTokenTo(_to, _tokenId);
+
+        Transfer(_from, _to, _tokenId);
+    }
+
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public canTransfer(_tokenId)
+    {
+        // solium-disable-next-line arg-overflow
+        safeTransferFrom(_from, _to, _tokenId, "");
+    }
+
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes _data) public canTransfer(_tokenId)
+    {
+        transferFrom(_from, _to, _tokenId);
+        // solium-disable-next-line arg-overflow
+        require(checkAndCallSafeTransfer(_from, _to, _tokenId, _data));
+    }
+
+
+    function isApprovedOrOwner(address _spender, uint256 _tokenId) internal view returns (bool)
+    {
+        address owner = ownerOf(_tokenId);
+        // Disable solium check because of
+        // https://github.com/duaraghav8/Solium/issues/175
+        // solium-disable-next-line operator-whitespace
+        return (
+        _spender == owner ||
+        getApproved(_tokenId) == _spender ||
+        isApprovedForAll(owner, _spender)
+        );
+    }
+
+
+    function _mint(address _to, uint256 _tokenId) internal {
+        require(_to != address(0));
+        addTokenTo(_to, _tokenId);
+        Transfer(address(0), _to, _tokenId);
+    }
+
+
+    function _burn(address _owner, uint256 _tokenId) internal {
+        clearApproval(_owner, _tokenId);
+        removeTokenFrom(_owner, _tokenId);
+        Transfer(_owner, address(0), _tokenId);
+    }
+
+
+    function clearApproval(address _owner, uint256 _tokenId) internal {
+        require(ownerOf(_tokenId) == _owner);
+        if (tokenApprovals[_tokenId] != address(0)) {
+            tokenApprovals[_tokenId] = address(0);
+            Approval(_owner, address(0), _tokenId);
+        }
+    }
+
+
+    function addTokenTo(address _to, uint256 _tokenId) internal {
+        require(tokenOwner[_tokenId] == address(0));
+        tokenOwner[_tokenId] = _to;
+        ownedTokensCount[_to] = ownedTokensCount[_to].add(1);
+    }
+
+
+    function removeTokenFrom(address _from, uint256 _tokenId) internal {
+        require(ownerOf(_tokenId) == _from);
+        ownedTokensCount[_from] = ownedTokensCount[_from].sub(1);
+        tokenOwner[_tokenId] = address(0);
+    }
+
+
+    function checkAndCallSafeTransfer(address _from, address _to, uint256 _tokenId, bytes _data) internal returns (bool)
+    {
+        if (!_to.isContract()) {
+            return true;
+        }
+        bytes4 retval = ERC721Receiver(_to).onERC721Received(
+        _from, _tokenId, _data);
+        return (retval == ERC721_RECEIVED);
+    }
+}
+
+
+
+
+contract ERC721Token is ERC721, ERC721BasicToken {
+    // Token name
+    string internal name_;
+
+    // Token symbol
+    string internal symbol_;
+
+    // Mapping from owner to list of owned token IDs
+    mapping(address => uint256[]) internal ownedTokens;
+
+    // Mapping from token ID to index of the owner tokens list
+    mapping(uint256 => uint256) internal ownedTokensIndex;
+
+    // Array with all token ids, used for enumeration
+    uint256[] internal allTokens;
+
+    // Mapping from token id to position in the allTokens array
+    mapping(uint256 => uint256) internal allTokensIndex;
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) internal tokenURIs;
+
+
+    function ERC721Token(string _name, string _symbol) public {
+        name_ = _name;
+        symbol_ = _symbol;
+    }
+
+
+    function name() public view returns (string) {
+        return name_;
+    }
+
+
+    function symbol() public view returns (string) {
+        return symbol_;
+    }
+
+
+    function tokenURI(uint256 _tokenId) public view returns (string) {
+        require(exists(_tokenId));
+        return tokenURIs[_tokenId];
+    }
+
+
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256)
+    {
+        require(_index < balanceOf(_owner));
+        return ownedTokens[_owner][_index];
+    }
+
+
+    function tokensOfOwner(address _owner) public view returns (uint256[])
+    {
+        return ownedTokens[_owner];
+    }
+
+
+    function totalSupply() public view returns (uint256) {
+        return allTokens.length;
+    }
+
+
+    function tokenByIndex(uint256 _index) public view returns (uint256) {
+        require(_index < totalSupply());
+        return allTokens[_index];
+    }
+
+
+    function _setTokenURI(uint256 _tokenId, string _uri) internal {
+        require(exists(_tokenId));
+        tokenURIs[_tokenId] = _uri;
+    }
+
+
+    function addTokenTo(address _to, uint256 _tokenId) internal {
+        super.addTokenTo(_to, _tokenId);
+        uint256 length = ownedTokens[_to].length;
+        ownedTokens[_to].push(_tokenId);
+        ownedTokensIndex[_tokenId] = length;
+    }
+
+
+    function removeTokenFrom(address _from, uint256 _tokenId) internal {
+        super.removeTokenFrom(_from, _tokenId);
+
+        uint256 tokenIndex = ownedTokensIndex[_tokenId];
+        uint256 lastTokenIndex = ownedTokens[_from].length.sub(1);
+        uint256 lastToken = ownedTokens[_from][lastTokenIndex];
+
+        ownedTokens[_from][tokenIndex] = lastToken;
+        ownedTokens[_from][lastTokenIndex] = 0;
+        // Note that this will handle single-element arrays. In that case, both tokenIndex and lastTokenIndex are going to
+        // be zero. Then we can make sure that we will remove _tokenId from the ownedTokens list since we are first swapping
+        // the lastToken to the first position, and then dropping the element placed in the last position of the list
+
+        ownedTokens[_from].length--;
+        ownedTokensIndex[_tokenId] = 0;
+        ownedTokensIndex[lastToken] = tokenIndex;
+    }
+
+
+    function _mint(address _to, uint256 _tokenId) internal {
+        super._mint(_to, _tokenId);
+
+        allTokensIndex[_tokenId] = allTokens.length;
+        allTokens.push(_tokenId);
+    }
+
+
+
+    function _burn(address _owner, uint256 _tokenId) internal {
+        super._burn(_owner, _tokenId);
+
+        // Clear metadata (if any)
+        if (bytes(tokenURIs[_tokenId]).length != 0) {
+            delete tokenURIs[_tokenId];
+        }
+
+        // Reorg all tokens array
+        uint256 tokenIndex = allTokensIndex[_tokenId];
+        uint256 lastTokenIndex = allTokens.length.sub(1);
+        uint256 lastToken = allTokens[lastTokenIndex];
+
+        allTokens[tokenIndex] = lastToken;
+        allTokens[lastTokenIndex] = 0;
+
+        allTokens.length--;
+        allTokensIndex[_tokenId] = 0;
+        allTokensIndex[lastToken] = tokenIndex;
+    }
+}
+
+
+
+contract BurnableToken is ERC721BasicToken {
+    function burn(uint256 _tokenId) public {
+        _burn(msg.sender, _tokenId);
+    }
+}
+
+
+contract MintableToken is ERC721Token, Ownable {
+    event MintFinished();
+
+    bool public mintingFinished = false;
+
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
+
+    function mint(address _to, uint256 _tokenId) public onlyOwner canMint {
+        _mint(_to, _tokenId);
+    }
+
+    function mintWithURI(address _to, uint256 _tokenId, string _uri) public onlyOwner canMint {
+        _mint(_to, _tokenId);
+        super._setTokenURI(_tokenId, _uri);
+    }
+
+
+    function finishMinting() onlyOwner canMint public returns (bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
+}
+
+
+contract CappedToken is MintableToken {
+    uint256 public cap;
+
+    function CappedToken(uint256 _cap) public {
+        require(_cap > 0);
+        cap = _cap;
+    }
+
+    function mint(address _to, uint256 _tokenId) onlyOwner canMint public {
+        require(totalSupply().add(1) <= cap);
+
+        return super.mint(_to, _tokenId);
+    }
+
+    function mintWithURI(address _to, uint256 _tokenId, string _uri) onlyOwner canMint public {
+        require(totalSupply().add(1) <= cap);
+
+        return super.mintWithURI(_to, _tokenId, _uri);
+    }
+
+}
+
+
+
+contract Token is ERC721Token , CappedToken, BurnableToken {
+    
+    function Token()
+        public
+        payable
+        ERC721Token('Leningrad', 'LND')
+        CappedToken(1000) {}
+    
+    
+    function setTokenURI(uint256 _tokenId, string _uri) external onlyOwnerOf(_tokenId) {
+        return super._setTokenURI(_tokenId, _uri);
+    }
+    
+    function delegateToken(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        return super.transferFrom(msg.sender,_to,_tokenId);
+    }
+    
+    function createToken(address _to,uint256 _tokenId, string _uri) public onlyOwner {
+        return mintWithURI(_to,_tokenId,_uri);
+    }
+    
+}
